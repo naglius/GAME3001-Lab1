3f1b68ffded3f587b95063ef2cfbbbe6e00904d1
naglius <italiannag98@gmail.com>
2021-01-20 15:50:32 -0500

    Test version of Lab1

---
+++ src/SpaceShip.cpp
@@ -0,0 +1,75 @@
+#include "SpaceShip.h"
+#include "Util.h"
+
+SpaceShip::SpaceShip():m_maxSpeed(10.0f) //----> set speeds directly
+{
+	TextureManager::Instance()->load("../Assets/textures/spaceship.png", "spaceship");
+
+	auto size = TextureManager::Instance()->getTextureSize("spaceship");
+	setWidth(size.x);
+	setHeight(size.y);
+
+	getTransform()->position = glm::vec2(400.0f, 300.0f);
+	getRigidBody()->velocity = glm::vec2(0.0f, 0.0f);
+	getRigidBody()->acceleration = glm::vec2(0.0f, 0.0f);
+	getRigidBody()->isColliding = false;
+	setType(SPACE_SHIP);
+	setOrientation(glm::vec2(0.0f, -1.0f));
+	setRotation(0.0f);
+	//setMaxSpeed(10.0f); ---> Accesses the setter
+}
+
+SpaceShip::~SpaceShip()
+= default;
+
+void SpaceShip::draw()
+{
+	TextureManager::Instance()->draw("spaceship", 
+		getTransform()->position.x, getTransform()->position.y, 0, 255, true);
+
+	Util::DrawLine(getTransform()->position, (getTransform()->position + m_orientation) * 60.0f); //Draw a line from where the ship is pointed to
+}
+
+void SpaceShip::update()
+{
+	m_Move();
+}
+
+void SpaceShip::clean()
+{
+}
+
+void SpaceShip::setDestination(const glm::vec2 destination)
+{
+	m_destination = destination;
+}
+
+void SpaceShip::setMaxSpeed(const float speed)
+{
+	m_maxSpeed = speed;
+}
+
+void SpaceShip::setOrientation(glm::vec2 orientation)
+{
+	m_orientation = orientation;
+}
+
+void SpaceShip::setRotation(float angle);
+{
+	m_rotationAngle = angle;
+}
+
+float SpaceShip::
+
+void SpaceShip::m_Move()
+{
+	//Magnitude
+	m_targetDirection = m_destination - getTransform()->position;
+
+	//normalized direction
+	m_targetDirection = Util::normalize(m_targetDirection);
+
+	getRigidBody()->velocity = m_targetDirection * m_maxSpeed;
+
+	getTransform()->position += getRigidBody()->velocity;
+}
